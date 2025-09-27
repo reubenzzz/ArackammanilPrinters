39a7206c3ae4a58925d9a5fd8cd39a90563b8676
Reuben <90466947+reubenzzz@users.noreply.github.com>
2025-09-27 22:53:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ProjectDetailPage from './components/ProjectDetail';
 import ScrollToTop from './components/ScrollToTop';
 import ContactSection from './components/ContactUs';
 import AboutPage from './components/AboutUsPage';
-import CategoriesSection from '@/components/CategoriesSection';
+import CategorySession from '@/components/CategorySession';
 const App = () => {
   return (
     <Router>
